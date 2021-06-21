e5947b817564a23fabc99f3294d29092652cbe50
msKoo <48276595+msKoo@users.noreply.github.com>
2021-06-21 20:47:14 +0900

    Update App.js

--- front/web_front/src/App.js
+++ front/web_front/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 import Contents from './components/Contents'
+import Post from './components/Post/PostMain'
 function App() {
   return (
     <div className="App">
       <Header />
       <Contents />
+      <Post />
     </div>
   );
 }
